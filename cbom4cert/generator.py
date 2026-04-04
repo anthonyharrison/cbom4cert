43ad8c0472422a66e4ae02a182084268349540f5
anthonyharrison <anthony.p.harrison@gmail.com>
2026-04-04 20:17:53 +0100

    chore: linting

--- cbom4cert/generator.py
+++ cbom4cert/generator.py
@@ -6,14 +6,14 @@ import platform
 import ssl
 import subprocess  # nosec
 import unicodedata
+import warnings
 from datetime import datetime, timezone
 from pathlib import Path
-import warnings
-from cryptography.utils import CryptographyDeprecationWarning
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec, rsa
+from cryptography.utils import CryptographyDeprecationWarning
 from lib4sbom.data.cryptography import SBOMCryptography
 from lib4sbom.data.package import SBOMPackage
 from lib4sbom.generator import SBOMGenerator
